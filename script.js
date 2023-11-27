a6e842f92cfbd21ce86f307f383ff9e686e73c73
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-11-27 11:23:19 +0400

    use scientific notation (exponent) to declare large and small numbers

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ console.log(typeof num);
 console.log(typeof decNum);
 
 // in javascript data type number deals with both integer and decimal numbers, so result will be number
+
+let mill = 1000000;
+console.log(mill);
+
+let expMill = 1e6;
+//1*(10**6) = 1000000
+console.log(expMill);
+
+let smallNum = 1e-6;
+console.log(smallNum);
