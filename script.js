444a478ce7e1fcde9766bb8491fbb0bc457718c7
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-11-27 11:18:50 +0400

    integer and decimal are both number type in javascript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('initial');
+let num = 3;
+let decNum = 3.14;
+
+console.log(typeof num);
+console.log(typeof decNum);
+
+// in javascript data type number deals with both integer and decimal numbers, so result will be number
