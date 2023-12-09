9beb839cf758c5f4d82c04cf111f6024efb412c7
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-09 12:35:01 +0400

    two sum function from leetcode

--- script.js
+++ script.js
@@ -597,3 +597,22 @@ for(let i =0;i<arg.length/2;i++){
     return true;
 }  
 }
+
+/////////////
+
+const twoSum = function(nums,target){
+    for(let i = 0; i<nums.length;i++){
+        console.log(i);
+        for(let j = 1; j<nums.length;j++){
+            console.log(j);
+            if(nums[i]+nums[j]===target && i!==j){
+                return [i,j];
+            }
+        }
+    }
+}
+console.log(twoSum([3,2,4],6));
+console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([3,3],6));
+console.log(twoSum([2,5,5,3],10));
+
