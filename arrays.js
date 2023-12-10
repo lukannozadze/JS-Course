f7f9b66d852ede6f1a244daebe27361c87e43564
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-10 23:14:01 +0400

    findMedian task from leetcode

--- arrays.js
+++ arrays.js
@@ -1,239 +1,262 @@
-//Arrays
-const myArray = [];
+// //Arrays
+// const myArray = [];
 
-//add elements to an array
-myArray[0] = 'Luka';
-myArray[1] = 21;
-myArray[2] = true;
+// //add elements to an array
+// myArray[0] = "Luka";
+// myArray[1] = 21;
+// myArray[2] = true;
 
-//refer to an array
-console.log(myArray);
+// //refer to an array
+// console.log(myArray);
 
-//length property
-console.log(myArray.length)
+// //length property
+// console.log(myArray.length);
 
-//last element in an array
-console.log(myArray[myArray.length-1]);
+// //last element in an array
+// console.log(myArray[myArray.length - 1]);
 
-console.log(myArray[1]);
-//add and remove elements from array
+// console.log(myArray[1]);
+// //add and remove elements from array
 
-//add and remove last element of an array
-myArray.push("school");
+// //add and remove last element of an array
+// myArray.push("school");
 
-console.log(myArray);
+// console.log(myArray);
 
-const lastItem = myArray.pop();
-console.log(lastItem);
+// const lastItem = myArray.pop();
+// console.log(lastItem);
 
-//add and remove first element of an array
-//add
-myArray.unshift(42);
-console.log(myArray);
+// //add and remove first element of an array
+// //add
+// myArray.unshift(42);
+// console.log(myArray);
 
-//remove
-myArray.shift();
-console.log(myArray);
+// //remove
+// myArray.shift();
+// console.log(myArray);
 
-console.log('initial');
+// console.log("initial");
 
-const equipShelfA = ['baseball','football','volleyball'];
-const equipShelfB = ['basketball','golf balls','tennis balls'];
+// const equipShelfA = ["baseball", "football", "volleyball"];
+// const equipShelfB = ["basketball", "golf balls", "tennis balls"];
 
-const clothesShelfA = ['tank tops','t-shirts','jerseys'];
-const clothesShelfB = ['sweat tops','sweat pants','hoodies'];
+// const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
+// const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
 
-//referencing an element in one dimensional array
-console.log(equipShelfA[1]);
-console.log(clothesShelfB[0]);
+// //referencing an element in one dimensional array
+// console.log(equipShelfA[1]);
+// console.log(clothesShelfB[0]);
 
-const equipDept = [equipShelfA,equipShelfB];
-const clothesDept = [clothesShelfA,clothesShelfB];
+// const equipDept = [equipShelfA, equipShelfB];
+// const clothesDept = [clothesShelfA, clothesShelfB];
 
-//referencing an element in two dimensional array
-console.log(equipDept[0][1]);
-console.log(equipDept[1][0]);
+// //referencing an element in two dimensional array
+// console.log(equipDept[0][1]);
+// console.log(equipDept[1][0]);
 
-const sportsStore = [equipDept,clothesDept];
+// const sportsStore = [equipDept, clothesDept];
 
-//referencing an element in three dimensional array
-console.log(sportsStore[1][0][1]);
+// //referencing an element in three dimensional array
+// console.log(sportsStore[1][0][1]);
 
-/////////////////////////////
+// /////////////////////////////
 
-const people = ['Sally','Kyle','John'];
+// const people = ["Sally", "Kyle", "John"];
 
-//mutate original array
-people[0] = 'Luka';
+// //mutate original array
+// people[0] = "Luka";
 
-//copy of people array
-const peopleCopy = [...people];
+// //copy of people array
+// const peopleCopy = [...people];
 
-//create copy and mutate so concept of immutability is protected
-peopleCopy[0] = 'Luka';
+// //create copy and mutate so concept of immutability is protected
+// peopleCopy[0] = "Luka";
 
-console.log(peopleCopy);
+// console.log(peopleCopy);
 
-//the same we can do with array.prototype.with function without mutating original array
+// //the same we can do with array.prototype.with function without mutating original array
 
-const newCopy=people.with(2,"Jeremy");
+// // const newCopy = people.with(2, "Jeremy");
 
-console.log(newCopy);
-///////////////////////////////////////////////
+// console.log(newCopy);
+// ///////////////////////////////////////////////
 
-const sortedPeople = [...people].sort();
+// const sortedPeople = [...people].sort();
 
-console.log('sorted',sortedPeople);
-console.log('original',people);
+// console.log("sorted", sortedPeople);
+// console.log("original", people);
 
+// //because of sort function does not return new array we should make copy of original array and then sort it
+// //this makes javascript iterate through array two times
+// //to solve this there is toSorted() method which returns new array and avoids us making a copy. good for performance
 
-//because of sort function does not return new array we should make copy of original array and then sort it
-//this makes javascript iterate through array two times
-//to solve this there is toSorted() method which returns new array and avoids us making a copy. good for performance
+// const sortedWithoutClone = people.toSorted();
+// console.log(sortedWithoutClone);
+// console.log(people);
 
-const sortedWithoutClone = people.toSorted();
-console.log(sortedWithoutClone);
-console.log(people);
+// //////
+// const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-//////
-const numArr =[1,2,3,4,5,6,7,8,9];
+// //map performs as requested and returns new array
+// const modifiedArr = numArr.map((item) => item * 2);
 
-//map performs as requested and returns new array
-const modifiedArr = numArr.map((item)=>item*2);
+// console.log(modifiedArr);
 
-console.log(modifiedArr);
+// //foreach does not mutate original array it performs requested action on each item
+// numArr.forEach((item) => console.log(item * 2));
 
-//foreach does not mutate original array it performs requested action on each item
-numArr.forEach((item)=>console.log(item*2));
+// ////////////////////////
+// //filter
+// const evens = numArr.filter((item) => item % 2 === 0);
 
-////////////////////////
-//filter
-const evens = numArr.filter((item)=>item%2===0)
+// console.log(evens);
 
-console.log(evens);
+// //concat
+// concatedArr = evens.concat(people);
+// console.log(concatedArr);
 
-//concat
-concatedArr =evens.concat(people);
-console.log(concatedArr);
+// //find
+// const special = numArr.find((element) => element % 3 === 0 && element % 6 == 0);
 
-//find
-const special = numArr.find((element)=>element%3===0 && element%6==0);
+// console.log(special);
 
-console.log(special);
+// //findIndex
 
-//findIndex
+// const specialIndex = numArr.findIndex(
+//   (element) => element % 3 === 0 && element % 6 === 0
+// );
+// console.log(numArr[specialIndex]);
 
-const specialIndex = numArr.findIndex((element)=>element%3===0 && element%6===0);
-console.log(numArr[specialIndex]);
+// //indexOf && lastIndexOf
+// const specIndex = numArr.indexOf(special);
+// console.log(specIndex);
+// console.log(numArr.indexOf(999));
 
-//indexOf && lastIndexOf
-const specIndex = numArr.indexOf(special);
-console.log(specIndex);
-console.log(numArr.indexOf(999));
+// numArr.unshift(9);
+// console.log(numArr);
 
-numArr.unshift(9);
-console.log(numArr);
+// //difference between indexOf and lastIndexOf is that indexof always refers first required element index but lastIndexOf refers last required index of element
+// console.log("IndexOf Method: ", numArr.indexOf(9));
+// console.log("LastIndexOf Method: ", numArr.lastIndexOf(9));
 
-//difference between indexOf and lastIndexOf is that indexof always refers first required element index but lastIndexOf refers last required index of element
-console.log('IndexOf Method: ', numArr.indexOf(9));
-console.log('LastIndexOf Method: ', numArr.lastIndexOf(9));
+// //some
+// //Array.prototype.some() function will check if requested condition fits to any element from given array and returns true or false
+// const hasEight = numArr.some((element) => element % 8 === 0);
+// console.log(hasEight); //Output: true;
 
-//some
-//Array.prototype.some() function will check if requested condition fits to any element from given array and returns true or false
-const hasEight = numArr.some((element)=>element%8===0);
-console.log(hasEight); //Output: true;
+// //every
+// //Array.prototype.every() function will check if requested condition fits to all element of the given array and returns true or false
+// isOddNums = numArr.every((element) => element % 2 === 1);
+// console.log(isOddNums); //Output: false;
 
-//every
-//Array.prototype.every() function will check if requested condition fits to all element of the given array and returns true or false
-isOddNums = numArr.every((element)=>element%2===1);
-console.log(isOddNums) //Output: false;
+// ///////////////////////////////////
 
-///////////////////////////////////
+// //includes
+// const hasTwo = numArr.includes(2);
+// console.log(hasEight);
 
-//includes
-const hasTwo = numArr.includes(2);
-console.log(hasEight);
+// //toString
+// const numString = numArr.toString();
+// console.log(numString);
 
-//toString
-const numString = numArr.toString();
-console.log(numString);
+// //join
+// const joinedStr = numArr.join("+");
+// console.log(joinedStr);
 
-//join
-const joinedStr = numArr.join('+');
-console.log(joinedStr);
+// //fill
+// numArr.fill(".", 2, 5);
+// console.log(numArr); //Output: 9,1,.,.,.,5,6,7,8,9
 
-//fill
-numArr.fill('.',2,5);
-console.log(numArr); //Output: 9,1,.,.,.,5,6,7,8,9
+// //slice
 
-//slice
+// const sliced = numArr.slice(2, 5);
+// console.log(sliced); //Output:['.','.','.']
 
-const sliced = numArr.slice(2,5);
-console.log(sliced); //Output:['.','.','.']
+// //splice
+// const months = ["Jan", "February", "March", "April"];
 
-//splice
-const months =['Jan','February','March','April'];
+// const newMonths = months.splice(1, 1, "August");
+// console.log(months);
 
-const newMonths = months.splice(1,1,'August');
-console.log(months);
+// //sort
+// const messedArr = [1, 5, 80, 9, 6];
+// messedArr.sort();
+// console.log(messedArr);
 
-//sort
-const messedArr = [1,5,80,9,6];
-messedArr.sort();
-console.log(messedArr);
+// messedArr.sort((a, b) => a - b); //asceding
+// console.log(messedArr);
 
-messedArr.sort((a,b)=>a-b) //asceding
-console.log(messedArr);
+// //from
+// str = "1234";
 
-//from
-str = '1234';
+// const nums = Array.from(str);
+// console.log(nums);
 
-const nums = Array.from(str);
-console.log(nums);
+// //isArray
+// const isArray = Array.isArray(nums);
+// console.log(isArray);
 
-//isArray
-const isArray = Array.isArray(nums);
-console.log(isArray);
+// //entries
+// console.log([...nums.entries()]);
 
-//entries
-console.log([...nums.entries()]);
+// //keys
+// numsArrkeys = [...nums.keys()];
+// console.log(numsArrkeys);
 
-//keys
-numsArrkeys = [...nums.keys()];
-console.log(numsArrkeys);
+// //values
+// numsArrValues = [...nums.values()];
+// console.log(numsArrValues);
+// console.log(nums);
 
-//values
-numsArrValues = [...nums.values()];
-console.log(numsArrValues);
-console.log(nums);
+// //reduce
+// //if we do not assign 0 or something as initial value
+// //default it will be 1
+// const numbers = [6, 6, 1, 2, 6, 21, 24, 2];
+// const sum = numbers.reduce((prev, cur) => prev + cur);
+// console.log(sum);
 
-//reduce
-//if we do not assign 0 or something as initial value
-//default it will be 1
-const numbers =[6,6,1,2,6,21,24,2];
-const sum = numbers.reduce((prev,cur)=>prev+cur);
-console.log(sum);
+// //flat
+// const n = [1, 2, 3, [4, [5, [6]]]];
 
-//flat
-const n = [1,2,3,[4,[5,[6]]]];
+// const newA = n.flat(1);
+// console.log(newA);
 
-const newA = n.flat(1);
-console.log(newA);
+// ///////
 
-///////
-
-const removeDuplicates = function(nums) {
-    nums.sort((a,b)=>a-b);
-    const newNums = [];
-    let dubQuantity = 0;
-    for(let i =0;i<nums.length;i++){
-        
-        if(nums[i]!==nums[i+1]){
-            newNums.push(nums[i]);
-        }else{
-            dubQuantity++;
-        }
+const removeDuplicates = function (nums) {
+  nums.sort((a, b) => a - b);
+  const newNums = [];
+  let dubQuantity = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      newNums.push(nums[i]);
+    } else {
+      dubQuantity++;
     }
-    console.log(`${dubQuantity} Duplicates found! There is Array with no duplicates [${newNums}]`);
+  }
+  console.log(
+    `${dubQuantity} Duplicates found! There is Array with no duplicates [${newNums}]`
+  );
 };
-removeDuplicates([1,3,4,1,1,4,5,12,2,4,5,2,21,2,3]);
+removeDuplicates([1, 3, 4, 1, 1, 4, 5, 12, 2, 4, 5, 2, 21, 2, 3]);
+
+/////////
+const findMedianSortedArrays = function (nums1, nums2) {
+  const mergedArr = [...nums1, ...nums2].sort((a, b) => a - b);
+  console.log(mergedArr);
+  const mergedArrLength = mergedArr.length;
+  if (mergedArrLength === 1) {
+    return mergedArr[0];
+  }
+  if (mergedArrLength % 2 === 1) {
+    return mergedArr[(mergedArrLength / 2 - 1).toFixed()];
+  } else {
+    return (
+      (mergedArr[(mergedArrLength / 2).toFixed()] +
+        mergedArr[(mergedArrLength / 2 - 1).toFixed()]) /
+      2
+    );
+  }
+};
+
+console.log(findMedianSortedArrays([], [1]));
