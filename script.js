0a4b4606c01fd074daba6e3321f7ce1a8bad83c0
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-10 11:27:31 +0400

    referencing an element in one two and three dimensional arrays

--- script.js
+++ script.js
@@ -36,3 +36,25 @@ myArray.shift();
 console.log(myArray);
 
 console.log('initial');
+
+const equipShelfA = ['baseball','football','volleyball'];
+const equipShelfB = ['basketball','golf balls','tennis balls'];
+
+const clothesShelfA = ['tank tops','t-shirts','jerseys'];
+const clothesShelfB = ['sweat tops','sweat pants','hoodies'];
+
+//referencing an element in one dimensional array
+console.log(equipShelfA[1]);
+console.log(clothesShelfB[0]);
+
+const equipDept = [equipShelfA,equipShelfB];
+const clothesDept = [clothesShelfA,clothesShelfB];
+
+//referencing an element in two dimensional array
+console.log(equipDept[0][1]);
+console.log(equipDept[1][0]);
+
+const sportsStore = [equipDept,clothesDept];
+
+//referencing an element in three dimensional array
+console.log(sportsStore[1][0][1]);
