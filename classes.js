641cda691c1a1edcedd2906daff5aaf433d6e97d
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-14 15:04:42 +0400

    getters and setters

--- classes.js
+++ classes.js
@@ -20,3 +20,29 @@ let myRectangle2 = new Rectangle(10, 5, "green");
 console.log(myRectangle1.getArea());
 console.log(myRectangle2.getArea());
 myRectangle2.printDescription();
+
+////////
+class Square {
+  constructor(_width) {
+    this.width = _width;
+    this.height = _width;
+    this.numOfRequestsForArea = 0;
+  }
+  get area() {
+    this.numOfRequestsForArea++;
+    return this.width * this.height;
+  }
+  set area(area) {
+    this.width = Math.sqrt(area);
+    this.height = this.width;
+  }
+}
+let square1 = new Square(4);
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+square1.area = 25;
+console.log(square1.height);
+console.log(square1.width);
+console.log(square1.numOfRequestsForArea);
