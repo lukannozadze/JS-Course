57d022cbc8e124882b65c0c2d4beaf1e2142a859
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-14 17:13:23 +0400

    inheritance and extends

--- classes.js
+++ classes.js
@@ -64,4 +64,38 @@ let square1 = new Square(8);
 let square2 = new Square(8);
 console.log(Square.equals(square1, square2));
 console.log(Square.isValidDimensions(6, 6));
-console.log(Square.test(1, 2));
+
+/////////////
+class Person {
+  constructor(_name, _age) {
+    this.name = _name;
+    this.age = _age;
+  }
+  describe() {
+    console.log(`I am ${this.name} and I am ${this.age} years old`);
+  }
+}
+class Programmer extends Person {
+  constructor(_name, _age, _yearsOfExperience) {
+    super(_name, _age);
+    this.yearsOfExperience = _yearsOfExperience;
+  }
+  code() {
+    console.log(`${this.name} is coding`);
+  }
+}
+const programmers = [
+  new Programmer("Luka", 21, 2),
+  new Programmer("Irakli", 26, 5),
+];
+
+let person1 = new Person("Jeff", 55);
+let programmer1 = new Programmer("Dom", 45, 12);
+programmer1.code();
+programmer1.describe();
+function developSoftware(programmers) {
+  for (let programmer of programmers) {
+    programmer.code();
+  }
+}
+developSoftware(programmers);
