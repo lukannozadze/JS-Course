060c153ef96a2496d0454ec2acc5f05ebc88f475
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-14 19:00:34 +0400

    some about classes again

--- classes.js
+++ classes.js
@@ -156,3 +156,29 @@ class ListBinding {
 }
 const listBinding = new ListBinding(myList);
 listBinding.update();
+///////////////////////////////
+class Pizza {
+  constructor(pizzaSize) {
+    this.size = pizzaSize;
+    this.crust = "original";
+  }
+  getCrust() {
+    return this.crust;
+  }
+  setCrust(pizzaCrust) {
+    this.crust = pizzaCrust;
+  }
+}
+
+class SpecialtyPizza extends Pizza {
+  constructor(pizzaSize) {
+    super(pizzaSize);
+    this.type = "The Works";
+  }
+  slice() {
+    console.log(`Our ${this.type} ${this.size} pizza has 8 slices`);
+  }
+}
+
+const mySpecialty = new SpecialtyPizza("large");
+mySpecialty.slice();
