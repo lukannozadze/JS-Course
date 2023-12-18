2d4c6bf29a8a71ef7bcf18e6e972ffe63476708b
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-18 13:52:54 +0400

    getter and setter as variables

--- getters-and-setters.js
+++ getters-and-setters.js
@@ -1,14 +1,15 @@
 person = {
   firstName: "luka",
   lastName: "nozadze",
-  getFullName() {
+  get fullName() {
     return `${this.firstName} ${this.lastName}`;
   },
-  setFullName(value) {
+  set fullName(value) {
     this.firstName = value.split(" ")[0];
     this.lastName = value.split(" ")[1];
   },
 };
 
-person.getFullName();
-person.setFullName("John Doe");
+console.log(person.fullName);
+person.fullName = "John Doe";
+console.log(person);
