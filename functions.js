034c98f2050d3aeea621f1b9a26967e2a84f787e
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-18 14:33:17 +0400

    difference between function declaration and function expression

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+print();
+x();
+//Difference between function declaration and function expression is hoisting function declaration always hoisted and function expression hoisted as
+//variable, so we can invoke variable as a function
 //Function Statement aka Function Declaration
 function print() {
   console.log("Namaste Kurkuma!");
