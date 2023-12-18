aa2ae976030bab368be687f612112143c376dcb4
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-18 13:45:29 +0400

    simple setter usecase

--- getters-and-setters.js
+++ getters-and-setters.js
@@ -4,6 +4,11 @@ person = {
   getFullName() {
     return `${this.firstName} ${this.lastName}`;
   },
+  setFullName(value) {
+    this.firstName = value.split(" ")[0];
+    this.lastName = value.split(" ")[1];
+  },
 };
 
 person.getFullName();
+person.setFullName("John Doe");
