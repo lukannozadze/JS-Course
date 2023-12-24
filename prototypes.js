8c4bd3c8a562c411753c087208c36df70da9ada8
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-24 17:52:01 +0400

    searching in prototype chaining

--- prototypes.js
+++ prototypes.js
@@ -10,3 +10,14 @@ console.log(arr.__proto__); //Array.prototype
 
 console.log(String.prototype);
 console.log(Number.prototype);
+
+/////////////////
+const human = {
+  kind: "human",
+};
+const luka = Object.create(human);
+luka.age = 21;
+console.log(luka);
+
+console.log(luka.kind); //firstly js searches kind property in object from it's called, when property is not found, js will search in it's prototype
+//(in our case human is luka's prototype)
