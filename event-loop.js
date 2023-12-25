5cb8f7f1ef0f764520730babb95a120da1d0a699
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-25 13:04:21 +0400

    event loop and event listeners

--- event-loop.js
+++ event-loop.js
@@ -13,3 +13,18 @@ console.log("End");
 //after 5 seconds callback function is sent to callback queue
 //event loop checks if call stack is empty, it will sent callback function in the call stack
 //callback function execution will start and callback will be printed in the console
+
+console.log("Start");
+document.getElementById("btn").addEventListener("click", () => {
+  console.log("Callback");
+});
+console.log("End");
+
+//in the call stack global execution context appears first
+//then first console log executes
+//after that addEventListener function is called and it is sent to web api
+//in web api callback function is registered and click event is attached to it
+//then second console log executes
+//after user click the button callback function is sent to callback queue
+//event loop checks, if call stack is empty callback function from callback queue will be sent to call stack and start executing
+//callback will be printed in the console
