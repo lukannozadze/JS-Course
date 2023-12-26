acfd7e49b4dd64baa444199a1e26e10d9f8bf39a
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-26 22:45:56 +0400

    promise does not block main thread execution

--- promises.js
+++ promises.js
@@ -31,8 +31,10 @@ const startTime = performance.now();
 console.log("synchronous 1");
 console.log(`sync1 - ${performance.now() - startTime}`);
 
-for (let i = 0; i < 10000000; i++) {}
-console.log(`for loop - ${performance.now() - startTime}`);
+Promise.resolve().then(() => {
+  for (let i = 0; i < 10000000; i++) {}
+  console.log(`for loop - ${performance.now() - startTime}`);
+});
 
 console.log("synchronous 2");
 console.log(`sync2 - ${performance.now() - startTime}`);
