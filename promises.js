7561b332ba6d2d9feebf2fe3120c8e8e9c4aaa68
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-26 22:41:04 +0400

    for loop blocks second console log to be executed

--- promises.js
+++ promises.js
@@ -1,24 +1,38 @@
-function getWeatcher(){
-    return new Promise(function(resolve,reject){
-         resolve('sunny');
-    })
+function getWeatcher() {
+  return new Promise(function (resolve, reject) {
+    resolve("sunny");
+  });
 }
-function onSuccess(data){
-    console.log(`Success ${data}`);
+function onSuccess(data) {
+  console.log(`Success ${data}`);
 }
-function onError(data){
-    console.log(`Error ${data}`);
+function onError(data) {
+  console.log(`Error ${data}`);
 }
-getWeatcher().then(onSuccess,onError);
-
+getWeatcher().then(onSuccess, onError);
 
 //
-const promise = fetch('https://jsonplaceholder.typicode.com/todos/1ss');
+// const promise = fetch("https://jsonplaceholder.typicode.com/todos/1ss");
 
-promise
- .then(response =>{
-    if(!response.ok){
-        throw new Error('Invalid response status');
-    }
-    return response.json();
- }).then(data=>console.log(data)).catch(error=>console.log(error));
+// promise
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Invalid response status");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+////
+
+const startTime = performance.now();
+
+console.log("synchronous 1");
+console.log(`sync1 - ${performance.now() - startTime}`);
+
+for (let i = 0; i < 10000000; i++) {}
+console.log(`for loop - ${performance.now() - startTime}`);
+
+console.log("synchronous 2");
+console.log(`sync2 - ${performance.now() - startTime}`);
