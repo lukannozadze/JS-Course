f510e19e400c4a897ef8a4fa42496bb8ec22ca78
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-26 17:18:43 +0400

    analize promise fullfiled and reject status with code

--- promises.js
+++ promises.js
@@ -10,3 +10,15 @@ function onError(data){
     console.log(`Error ${data}`);
 }
 getWeatcher().then(onSuccess,onError);
+
+
+//
+const promise = fetch('https://jsonplaceholder.typicode.com/todos/1ss');
+
+promise
+ .then(response =>{
+    if(!response.ok){
+        throw new Error('Invalid response status');
+    }
+    return response.json();
+ }).then(data=>console.log(data)).catch(error=>console.log(error));
