e4ef41314fb70aff8c025fdef2dd5bdb9fe0bee6
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-26 11:12:51 +0400

    main thread,micro and macro tasks execution order

---
+++ micro-macro.js
@@ -0,0 +1,25 @@
+console.log('A Stack');
+
+queueMicrotask(() => console.log('B Queue Microtask'));
+
+requestAnimationFrame(() => console.log('C Request Animation Frame'));
+
+console.log('D Stack');
+
+setTimeout(() => console.log('E Timeout'), 0);
+
+console.log('F Stack');
+
+Promise.resolve().then(() => console.log('G Promise'));
+
+console.log('H Stack');
+
+requestAnimationFrame(() => console.log('I Request Animation Frame'));
+
+console.log('J Stack');
+
+setTimeout(() => console.log('K Timeout'), 0);
+
+queueMicrotask(() => console.log('L Queue Microtask'));
+
+console.log('M Stack');
