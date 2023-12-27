5062302a5c8836e8562de80908524237a12216a5
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-27 17:19:13 +0400

    url,request,headers

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,6 +1,6 @@
 //the simplest fetch you can use and still have error handling
 
-const url = "https://jsonplaceholder.typicode.com/usersasdasd";
+// const url = "https://jsonplaceholder.typicode.com/usersasdasd";
 
 // function getData(){
 //     fetch(url)
@@ -19,17 +19,42 @@ const url = "https://jsonplaceholder.typicode.com/usersasdasd";
 //  }
 // getData();
 
-async function getData(){
-    try{
-        let response = await fetch(url);
-        if(!response.ok){
-            throw new Error("Invalid response status");
-        }
-        console.log(response);
-        let data = await response.json();
+// const url = "https://jsonplaceholder.typicode.com/usersasdasd";
+
+// async function getData(){
+//     try{
+//         let response = await fetch(url);
+//         if(!response.ok){
+//             throw new Error("Invalid response status");
+//         }
+//         console.log(response);
+//         let data = await response.json();
+//         console.log(data);
+//     } catch(err){
+//         console.warn(err.message);
+//     }
+// }
+// getData();
+
+const str = 'https://jsonplaceholder.typicode.com/users';
+function getData(){
+    const url = new URL(str);
+    const request = new Request(url,{
+        headers:{'x-steve':'hello'},
+        method:'GET',
+        cache:'no-store'
+    });
+    console.log(url,url.hostname);
+    fetch(request).then(response=>{
+       if(!response.ok){
+        throw new Error("Invalid response status");
+       }
+       return response.json();
+    }).then(data=>{
         console.log(data);
-    } catch(err){
-        console.warn(err.message);
-    }
-}
+    })
+     .catch(err=>{
+         console.warn(err.message);
+     });
+} 
 getData();
