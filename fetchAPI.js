527cbc219b98149306353152cc583dfc8f382ff3
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-12-27 16:36:42 +0400

    modern way of dealing async operations(async/await)

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,20 +1,35 @@
 //the simplest fetch you can use and still have error handling
 
-const url = "https://jsonplaceholder.typicode.com/users";
+const url = "https://jsonplaceholder.typicode.com/usersasdasd";
 
-function getData(){
-    fetch(url)
-    .then(response=>{
+// function getData(){
+//     fetch(url)
+//     .then(response=>{
+//         if(!response.ok){
+//             throw new Error("Invalid response status");
+//         }
+//         return response.json(); //mehod to extra
+//     })
+//     .then(data=>{
+//         console.log(data);
+//     })
+//     .catch(err=>{
+//         console.warn(err.message);
+//     });
+//  }
+// getData();
+
+async function getData(){
+    try{
+        let response = await fetch(url);
         if(!response.ok){
             throw new Error("Invalid response status");
         }
-        return response.json(); //mehod to extra
-    })
-    .then(data=>{
+        console.log(response);
+        let data = await response.json();
         console.log(data);
-    })
-    .catch(err=>{
+    } catch(err){
         console.warn(err.message);
-    });
- }
+    }
+}
 getData();
