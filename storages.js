b73739ab7042da8680ab2b2ec89c9aaa60c42a0b
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-06 15:42:33 +0400

    a little about document.cookie

--- storages.js
+++ storages.js
@@ -19,3 +19,15 @@ localStorage.removeItem('name');
 
 sessionStorage.setItem('name','Luka');
 console.log(sessionStorage.getItem('name'));
+
+
+///////////////////////
+
+document.cookie = 'name=someone; expires=' + new Date(2025, 0, 1).toUTCString();
+
+document.cookie = 'lastName=Smith; expires=' + new Date(2025, 0, 1).toUTCString();
+
+document.cookie = 'other='+JSON.stringify(person) + ';' + 'expires' + new Date(2026,0,5).toUTCString();
+
+
+console.log(document.cookie)
