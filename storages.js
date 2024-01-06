93447569d390cb848a8fd7ed445f45ef13bb4ea8
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-06 15:17:54 +0400

    basic manipulations on localStorage

---
+++ storages.js
@@ -0,0 +1,15 @@
+localStorage.setItem('name','Bob');
+localStorage.setItem('lastname','Smith');
+localStorage.setItem('age','32');
+console.log(localStorage.getItem('name'));
+
+
+const person = {
+    name: 'John',
+    lastname: 'Doe',
+    age: 45
+};
+
+localStorage.setItem('person',JSON.stringify(person));
+console.log(JSON.parse(localStorage.getItem('person')))
+localStorage.removeItem('name');
