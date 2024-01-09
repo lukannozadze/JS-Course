9429e1d8c4a41862b5f002b2fb24e226b269680f
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-09 15:21:45 +0400

    little work with generators

--- generators.js
+++ generators.js
@@ -1,9 +1,34 @@
 function* generateId(){
+    let id = 1;
+
     while(true){
-        yield  window.crypto.randomUUID();
+       console.log(2);
+       const increment = yield id
+       console.log(increment);
+       if(increment!=null){
+        console.log('babiddy');
+        id+=increment
+       }else{
+        console.log('gigiddy')
+        id++;
+       }
     }
 }
 
 const generateObj = generateId();
+
 console.log(generateObj.next());
-console.log(generateObj.next());
+ console.log(generateObj.next(5));
+ console.log(generateObj.next());
+
+function* test(){
+    let id = 1; //5
+    while(true){
+        id = yield id;
+    }
+}
+
+// const t1 = test();
+// console.log(t1.next());
+// console.log(t1.next(7));
+// console.log(t1.next(10));
