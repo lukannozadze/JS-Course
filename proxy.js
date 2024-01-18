4b6de79b9119edad5dd2f3bf4f19cdc901a8305b
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-18 18:17:32 +0400

    use set property of proxy hanlder object

--- proxy.js
+++ proxy.js
@@ -3,6 +3,12 @@ const testProxy = new Proxy(test, {
   get: (obj, property) => {
     return property in obj ? obj[property] : "nothing found bratha";
   },
+  set: (obj, property, value) => {
+    property in obj ? (obj[property] = value) : "ver daemata";
+  },
 });
 console.log(testProxy.name);
+console.log(testProxy.age);
+
+testProxy.age = 1000;
 console.log(testProxy.age);
