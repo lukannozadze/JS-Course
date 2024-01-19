b150db53ecb489fa9293007ddb5efaba824315d3
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-19 14:36:56 +0400

    final commit

--- proxy.js
+++ proxy.js
@@ -1,14 +1,14 @@
-const test = { name: "luka" };
+const test = { name: "luka", age: 20 };
 const testProxy = new Proxy(test, {
   get: (obj, property) => {
     return property in obj ? obj[property] : "nothing found bratha";
   },
   set: (obj, property, value) => {
-    property in obj ? (obj[property] = value) : "ver daemata";
+    property in obj && value < 30
+      ? (obj[property] = value)
+      : console.error("Asaki unda iyos below 30");
   },
 });
-console.log(testProxy.name);
-console.log(testProxy.age);
 
-testProxy.age = 1000;
+testProxy.age = 55;
 console.log(testProxy.age);
